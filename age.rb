9f296560e186b6d23774773f2bb39e939a67c1a2
vipinkattungal <vipinkattungal@gmail.com>
2016-08-22 15:32:26 +0530

    edited

--- age.rb
+++ age.rb
@@ -1,13 +1,15 @@
 age = gets.to_i
 
-if age < 10
-	puts"its a kid"
-elsif age < 25
-	puts"its young person"
-elsif age < 45
-	puts"middle man"
-elsif age < 95
-	puts"old person"
-    
-end
+output= if age < 10
+		"its a kid"
+		elsif age < 25
+			"its young person"
+		elsif age < 45
+			"middle man"
+		elsif age < 95
+			"old person"	
+		else
+		   "he is alive?"    
+		end
 
+puts output
