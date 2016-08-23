278136a574a5716d2de9b9100e95aa0710dd7675
vipinkattungal <vipinkattungal@gmail.com>
2016-08-23 11:38:26 +0530

    ruby simple class programme

---
+++ classcar.rb
@@ -0,0 +1,28 @@
+class Car                       #class defined
+attr_reader :state		#attribute for methode
+def initialize engin,wheel	#initialize var
+@engin = engin			#declayer var
+@wheel = wheel
+end
+
+def start			#methode start
+state ="running"		#methode state
+puts"has started"
+end
+
+def stopped			#methode start
+state ="stopped"
+puts "has stopped"
+end
+end
+
+car = Car.new "very good engin",[1,2,3,4]	#object declaraton
+puts car.inspect			#inspect for inspct the momory loc
+car.start				#calling methode
+puts car.state
+car.stopped
+puts car.state
+
+
+
+
