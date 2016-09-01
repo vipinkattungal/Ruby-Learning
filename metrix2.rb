60f58cfd1d0fa8dcf923a4d865a7d3209cd55b45
vipinkattungal <vipinkattungal@gmail.com>
2016-09-01 18:03:29 +0530

    matrix pgm using globel variable

--- metrix2.rb
+++ metrix2.rb
@@ -1,14 +1,16 @@
+
+	def mat
+		print "#"," "*$sl,"#"
+	end
+	
 	print"enter side of metrix :"
 	hash = gets.to_i
 	print "# "*hash + 	"\n"
-   
+	
 	$sl = (hash*2)-3
 	$rl = hash-2
-   
-   def mat
-   print "#"," "*$sl,"#"
-   end
-   $rl.times do puts mat
+	
+	$rl.times do puts mat
 
-   end	
-   print "# "*hash + 	"\n"	
+	end	
+	print "# "*hash + 	"\n"	
