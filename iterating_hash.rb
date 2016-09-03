7f769f02456582de63c9572c26303dfd3c5778c1
vipinkattungal <vipinkattungal@gmail.com>
2016-09-03 17:34:38 +0530

    iterated hash function

---
+++ iterating_hash.rb
@@ -0,0 +1,10 @@
+secret_identities = {
+"the batman" => "bruce wayne",
+"superman" => "clark kent",
+"iron man" => "tony stark",
+"caption america" => "Steve Rogers"
+}
+secret_identities.each do |hero,real|
+puts "#{hero}: #{real}
+end
+
