83070437abf35ea0533da524305bc8aafc39de57
vipinkattungal <vipinkattungal@gmail.com>
2016-09-27 17:56:50 +0530

    hash itreation ,sorting with space

---
+++ hash_sort_itretion.rb
@@ -0,0 +1,10 @@
+puts 'enter name'
+text = gets.chomp
+words = text.split(" ")
+frequencies = Hash.new{0}
+words.each{|word| frequencies[word] += 1}
+frequencies = frequencies.sort_by {|k, v| v}
+frequencies.reverse!
+frequencies.each do |word, frequency|
+ puts word + " " + frequency.to_s
+ end
