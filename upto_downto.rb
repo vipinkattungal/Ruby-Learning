52cfbff2e5cd1f39f281da83dd155caf42f33da9
vipinkattungal <vipinkattungal@gmail.com>
2016-10-01 17:14:51 +0530

    to find out a range of number or alfabet

---
+++ upto_downto.rb
@@ -0,0 +1,2 @@
+"l".upto("z"){|letter| puts letter}               #to print rang of l to z ------upcase
+100.downto(0){|num| puts num}                        #to print value of 100 to 0 -------- downcase 
