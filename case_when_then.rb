1343f3b6c02918b4d875694757c2dbd74e1f3319
vipinkattungal <vipinkattungal@gmail.com>
2016-10-01 17:05:15 +0530

    case statement using when and then

---
+++ case_when_then.rb
@@ -0,0 +1,10 @@
+puts "Hello there!"
+greeting = gets.chomp
+
+case greeting
+when "English" then puts "Hello!"
+when "French" then puts "Bonjour!"
+when "German" then puts "Guten Tag!"
+when "Finnish" then puts "Haloo!"
+else puts "I don't know that language!"
+end
