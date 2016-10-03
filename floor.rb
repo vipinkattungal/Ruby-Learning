899e2c7f18415d08c82e89def310e7ed8f283023
vipinkattungal <vipinkattungal@gmail.com>
2016-10-03 19:27:43 +0530

    float (a number with a decimal) down to the nearest integer.

---
+++ floor.rb
@@ -0,0 +1,9 @@
+  # .floor = float (a number with a decimal) down to the nearest integer.
+  # The .collect! and .map! methods do the exact same thing
+
+floats = [1.2, 3.45, 0.91, 7.727, 11.42, 482.911]
+# Write your code below this line!
+round_down = Proc.new{|n|n.floor}
+
+
+# Write your code above this line!
